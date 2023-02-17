ea85dce84720eb9741401b89c7f1b3d9ef0c9633
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-02-17 12:39:22 -0500

    Validar lo que ocurre cuanto se intenta validar una tienda

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d779b73df4992edffe0d29aabc9cd805",
-"index.html": "316ac2afada20e792f9d07bb7b302dc9",
-"/": "316ac2afada20e792f9d07bb7b302dc9",
-"main.dart.js": "428fcf62b2dade86f9093854307f483c",
+"index.html": "a725a3adfd3faea11da72490261f3dbd",
+"/": "a725a3adfd3faea11da72490261f3dbd",
+"main.dart.js": "0df2758e70fc7d5b258ef3972f537919",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
