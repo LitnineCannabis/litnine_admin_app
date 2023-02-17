d975e36e8a00e5e734c3e1a4e7bab2eddd1169b3
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-02-17 13:03:37 -0500

    Eliminacion de href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d779b73df4992edffe0d29aabc9cd805",
-"index.html": "6b6aac95bc9d45e14d570466efc9aa61",
-"/": "6b6aac95bc9d45e14d570466efc9aa61",
-"main.dart.js": "bdc2bd37776d2ab82b3e986ad4f26076",
+"index.html": "99f93809d2334a1b8e66161f109f2ab3",
+"/": "99f93809d2334a1b8e66161f109f2ab3",
+"main.dart.js": "817378d69a9a615cffb0914286170939",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
